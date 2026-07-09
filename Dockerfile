6f560a3afb8419cd8e54d002b086235294977d74
AtaberkBY <yilmazataberkberke@gmail.com>
2026-07-09 19:01:52 +0300

    feat(core): initialize nestjs architecture with core modules and multistage dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# --- build stage ---
+FROM node:20-alpine AS builder
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+
+# --- production stage ---
+FROM node:20-alpine AS runner
+WORKDIR /app
+ENV NODE_ENV=production
+COPY package*.json ./
+RUN npm install --only=production
+COPY --from=builder /app/dist ./dist
+
+EXPOSE 3000
+CMD ["node", "dist/main.js"]
---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { OrdersModule } from './orders/orders.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, TicketsModule, OrdersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
